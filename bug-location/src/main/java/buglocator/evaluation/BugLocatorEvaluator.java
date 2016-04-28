2658b2e8d108fba26d8f73c3f88b1645cde0105b
juan <jmflorezff@gmail.com>
2016-04-28 10:08:31 -0500

    Implemented all evaluation statistics.

--- bug-location/src/main/java/buglocator/evaluation/BugLocatorEvaluator.java
+++ bug-location/src/main/java/buglocator/evaluation/BugLocatorEvaluator.java
@@ -22,24 +22,17 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
- * Created by juan on 4/25/16.
+ * Retrieval evaluator for the BugLocator approach.
  */
 public class BugLocatorEvaluator {
     //    private static final List<String> systems = Arrays.asList("bookkeeper", "derby", "lucene",
 //            "mahout", "openjpa", "pig", "solr", "tika", "zookeeper");
-    private static final String[] systems = {"swt", "aspectj", "eclipse"};
+    private static final String[] systems = {"swt", "aspectj"};//, "eclipse"};
 
-    public static void main(String[] args) throws IOException {
-        evaluate(BugLocatorRetriever.UseField.TITLE_AND_DESCRIPTION,
-                Paths.get("..", "index"),
-                Paths.get("..", "data"),
-                0.2F);
-    }
-
-    public static EvaluationResult[] evaluate(BugLocatorRetriever.UseField useField,
-                                              Path indexPath,
-                                              Path dataPath,
-                                              float alpha)
+    public EvaluationResult[] evaluate(BugLocatorRetriever.UseField useField,
+                                       Path indexPath,
+                                       Path dataPath,
+                                       float alpha)
             throws IOException {
 
         // Create a JSON deserializer
@@ -49,6 +42,7 @@ public class BugLocatorEvaluator {
         Gson gson = gsonBuilder.create();
 
         EvaluationResult[] results = new EvaluationResult[systems.length];
+        int systemIndex = 0;
 
         for (String system : systems) {
             Path sourceIndexPath = Paths.get(indexPath.toString(), "source-code", system);
@@ -77,16 +71,31 @@ public class BugLocatorEvaluator {
                     (Integer) collectionExtrema[1]);
 
             int top1s = 0;
-            int queries = 0;
+            int top5s = 0;
+            int top10s = 0;
+            // Amount of actually performed queries. A query will not be performed if the
+            // corresponding bug report doesn't have the required field specified in the UseField
+            // parameter of this method.
+            int actualQueries = 0;
+            float reciprocalRankAccum = 0;
+            float averagePrecisionAccum = 0;
+            float precisionAccum = 0;
+            float recallAccum = 0;
             int totalQueries = bugReportSearcher.getIndexReader().maxDoc();
 
             for (String jsonLine : FileUtils.readLines(Paths.get(dataPath.toString(),
                     "processed-bug-reports", system + ".json").toFile())) {
-                queries++;
                 System.out.println(
-                        String.format("Processing query %d of %d", queries, totalQueries));
+                        String.format("Processing query %d of %d", actualQueries + 1, totalQueries));
                 BugReport bugReport = gson.fromJson(jsonLine, BugReport.class);
+
                 ScoreDoc[] scoredFiles = retriever.locate(bugReport);
+                // If the bug report doesn't have the required information it will return null
+                if (scoredFiles == null) {
+                    continue;
+                }
+
+                actualQueries++;
                 Set<Integer> goldSet = new HashSet<>(bugReport.getFixedFiles()
                         .stream()
                         .map(f -> {
@@ -98,12 +107,100 @@ public class BugLocatorEvaluator {
                             return null;
                         }).collect(Collectors.toList()));
 
-                top1s += goldSet.contains(scoredFiles[0].doc) ? 1 : 0;
+                int topRank = getTopRank(goldSet, scoredFiles);
+
+                if (topRank <= 10) {
+                    top10s++;
+                    if (topRank <= 5) {
+                        top5s++;
+                        if (topRank == 1) {
+                            top1s++;
+                        }
+                    }
+                }
+
+                reciprocalRankAccum += topRank > 0 ? 1F / topRank : 0;
+                averagePrecisionAccum += calculateAveragePrecision(goldSet, scoredFiles);
+                int relevantRetrieved = getRelevantRetrieved(goldSet, scoredFiles);
+                precisionAccum += relevantRetrieved / ((float) scoredFiles.length);
+                recallAccum += relevantRetrieved / ((float) goldSet.size());
             }
 
-            float top1Precision = (float) top1s / queries;
+            if (actualQueries > 0) {
+                float top1Precision = (float) top1s / actualQueries;
+                float top5Precision = (float) top5s / actualQueries;
+                float top10Precision = (float) top10s / actualQueries;
+                float meanReciprocalRank = reciprocalRankAccum / actualQueries;
+                float meanAveragePrecision = averagePrecisionAccum / actualQueries;
+                float averagePrecision = precisionAccum / actualQueries;
+                float averageRecall = recallAccum / actualQueries;
+
+                results[systemIndex++] = new EvaluationResult(
+                        system,
+                        alpha,
+                        actualQueries,
+                        top1Precision,
+                        top5Precision,
+                        top10Precision,
+                        meanReciprocalRank,
+                        meanAveragePrecision,
+                        averagePrecision,
+                        averageRecall);
+            } else {
+                results[systemIndex++] = new EvaluationResult(system);
+                System.out.println("No valid queries for system " + system);
+            }
         }
 
-        return null;
+        return results;
+    }
+
+    private int getRelevantRetrieved(Set<Integer> goldSet, ScoreDoc[] scoredFiles) {
+        return (int) Arrays.stream(scoredFiles).filter(sd -> goldSet.contains(sd.doc)).count();
+    }
+
+    private float calculateAveragePrecision(Set<Integer> goldSet, ScoreDoc[] scoredFiles) {
+        float totalRelevant = Math.min(goldSet.size(), scoredFiles.length);
+        if (totalRelevant == 0) {
+            return 0;
+        }
+
+        float currentRelevant = 0;
+
+        float accumulator = 0;
+        for (int i = 0; i < scoredFiles.length; i++) {
+            // If the current document is relevant
+            if (goldSet.contains(scoredFiles[i].doc)) {
+                currentRelevant++;
+                accumulator += currentRelevant / (i + 1);
+            }
+
+            if (currentRelevant == totalRelevant) {
+                break;
+            }
+        }
+
+        return accumulator / totalRelevant;
+    }
+
+    /**
+     * Finds the first returned file belonging to the gold set.
+     *
+     * @param goldSet     A set of file IDs known to be the cause of this bug.
+     * @param scoredFiles The ranked files returned by the approach.
+     * @return The rank of the first file returned that is found in the gold set or {@code 0} if it
+     * is not found.
+     */
+    private int getTopRank(Set<Integer> goldSet, ScoreDoc[] scoredFiles) {
+        int topRank = 0;
+
+        for (int i = 0; i < scoredFiles.length; i++) {
+            if (goldSet.contains(scoredFiles[i].doc)) {
+                topRank = i + 1;
+                break;
+            }
+        }
+
+        return topRank;
     }
 }
